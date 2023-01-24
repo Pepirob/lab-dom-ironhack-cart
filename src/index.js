aacc23c415a1fc04ea7fa40398f115e618440716
Jose Luis <pepirob>
2023-01-24 19:39:48 +0100

    lab completo sin bonus

--- src/index.js
+++ src/index.js
@@ -2,22 +2,48 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const subtotalDOM = product.querySelector(".subtotal span")
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector(".quantity input");
+  let priceValue = Number(price.innerText);
+  let quantityValue = quantity.value;
+  let subtotal = priceValue * quantityValue;
 
-  //... your code goes here
+  subtotalDOM.innerText = subtotal;
 }
 
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // // code in the following two lines is added just for testing purposes.
+  // // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // // // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let productsDOM = document.querySelectorAll(".product");
+  
 
+  for (let i = 0; i < productsDOM.length; i++) {
+    
+    updateSubtotal(productsDOM[i]);
+    
+  }
+  
+  
   // ITERATION 3
-  //... your code goes here
+  let totalDom = document.querySelector("#total-value"); 
+  let subtotalDOM = document.querySelectorAll(".subtotal span")
+  let total = 0;
+  for (let i = 0; i < subtotalDOM.length; i++) {
+     total += Number(subtotalDOM[i].innerText)
+     
+    }
+  
+  totalDom.innerText = total;
+
+
+  
 }
 
 // ITERATION 4
