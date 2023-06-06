7235338d83f7df8396179e15eb78cc5c133fa081
Jose Luis <pepirob>
2023-06-06 11:01:15 +0200

    Revert "Revert "lab completo sin bonus""
    This reverts commit ab05a23ff016a02bfe3ea581e06c9fb793bc11c6.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,57 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log("Calculating subtotal, yey!");
+  console.log('Calculating subtotal, yey!');
+  const subtotalDOM = product.querySelector(".subtotal span")
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector(".quantity input");
+  let priceValue = Number(price.innerText);
+  let quantityValue = quantity.value;
+  let subtotal = priceValue * quantityValue;
 
-  //... your code goes here
+  subtotalDOM.innerText = subtotal;
 }
 
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector(".product");
-  updateSubtotal(singleProduct);
-  // end of test
+  // // code in the following two lines is added just for testing purposes.
+  // // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // // // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let productsDOM = document.querySelectorAll(".product");
+  
 
+  for (let i = 0; i < productsDOM.length; i++) {
+    
+    updateSubtotal(productsDOM[i]);
+    
+  }
+  
+  
   // ITERATION 3
-
-  let totalDOM = document.querySelector("#total-value");
-  let subtotalDOM = document.querySelectorAll(".subtotal span");
+  let totalDOM = document.querySelector("#total-value"); 
+  let subtotalDOM = document.querySelectorAll(".subtotal span")
   let total = 0;
   for (let i = 0; i < subtotalDOM.length; i++) {
-    total += Number(subtotalDOM[i].innerText);
-  }
-
+     total += Number(subtotalDOM[i].innerText)
+     
+    }
+  
   totalDOM.innerText = total;
 
-  //... your code goes here
+
+  
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
+  
   const target = event.currentTarget;
-  console.log("The target in remove is:", target);
+  console.log('The target in remove is:', target);
 
   target.parentNode.parentNode.remove();
 }
@@ -44,24 +61,26 @@ function removeProduct(event) {
 function createProduct() {
   //... your code goes here
   // console.log("probando")
-  const inputsDOM = document.querySelectorAll(".create-product input");
+  const inputsDOM = document.querySelectorAll('.create-product input')
   const nameInputDOM = inputsDOM.item(0);
   const priceInputDOM = inputsDOM.item(1);
   text = nameInputDOM.value;
   price = priceInputDOM.value;
+  
 
   // console.log(nameInputDOM, priceInputDOM)
 }
 
-window.addEventListener("load", () => {
-  const calculatePricesBtn = document.getElementById("calculate");
-  calculatePricesBtn.addEventListener("click", calculateAll);
+window.addEventListener('load', () => {
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
 
   let allRemoveDOM = document.querySelectorAll(".btn-remove");
-  allRemoveDOM.forEach((btn) => {
+  allRemoveDOM.forEach( (btn) => {
     btn.addEventListener("click", removeProduct);
-  });
+  })
 
   const createBtnDOM = document.querySelector("#create");
-  createBtnDOM.addEventListener("click", createProduct);
+  createBtnDOM.addEventListener('click', createProduct)
+  
 });
